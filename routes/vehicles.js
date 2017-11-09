a8bac66ec3d58a33dc6056d1d1ffa578cec0a0d7
Andrew <support@astech-it.co.uk>
2017-11-09 11:13:54 +0000

    v1.3.3 Added search functionality page for vehicle search

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -1,10 +1,9 @@
-// INDEX route - show all campgrounds
 var express=require("express");
 var router = express.Router();
 var Vehicle = require("../models/vehicle");
 var middleware = require("../middleware");
 
-// INDEX ROUTE
+// INDEX VEHICLE ROUTE
 router.get("/", function(req, res) {
     Vehicle.find({}, function(err, allVehicles) {
         if (err) {
@@ -16,10 +15,16 @@ router.get("/", function(req, res) {
     });
 });
 
-// CREATE ROUTE
+// SEARCH VEHICLE ROUTE
+router.get("/search", function(req, res) {
+            res.render("vehicles/search.ejs");
+    });
+
+
+// CREATE VEHICLE ROUTE
 
 router.post("/", middleware.isLoggedIn, function(req, res) {
-    //get data from form and add to campgrounds array
+    //get data from form and add to vehicles array
     var make = req.body.make;
     var model = req.body.model;
     var transmission = req.body.transmission;
@@ -33,7 +38,7 @@ router.post("/", middleware.isLoggedIn, function(req, res) {
     }
     
     var newVehicle = { make: make, model: model, transmission: transmission, fuel_type: fuel_type, image: image, description: desc, author: author, price: price };
-    // Add new campground to database
+    // Add new vehicle to database
     Vehicle.create(newVehicle, function(err, newlyCreated) {
         if (err) {
             console.log(err);
@@ -45,13 +50,13 @@ router.post("/", middleware.isLoggedIn, function(req, res) {
 
 });
 
-// NEW ROUTE
+// NEW VEHICLE ROUTE
 
 router.get("/new", middleware.isLoggedIn, function(req, res) {
     res.render("vehicles/new.ejs");
 });
 
-// SHOW ROUTE
+// SHOW VEHICLE ROUTE
 
 router.get("/:id", function(req, res) {
     Vehicle.findById(req.params.id).populate("comments").exec(function(err, foundVehicle) {
@@ -63,7 +68,7 @@ router.get("/:id", function(req, res) {
         }
     });
 });
-//EDIT CAMPGROUND ROUTE
+//EDIT VEHICLE ROUTE
 
 router.get("/:id/edit", middleware.checkVehicleOwnership, function(req,res){
             Vehicle.findById(req.params.id, function(err, foundVehicle){
@@ -71,7 +76,7 @@ router.get("/:id/edit", middleware.checkVehicleOwnership, function(req,res){
     });
 });
 
-//UPDATE CAMPGROUND ROUTE
+//UPDATE VEHICLE ROUTE
 
 router.put("/:id", middleware.checkVehicleOwnership, function(req,res){
    // find and update the correct campground
@@ -80,7 +85,7 @@ router.put("/:id", middleware.checkVehicleOwnership, function(req,res){
     });
 });
 
-// DESTROY CAMPGROUND ROUTE
+// DESTROY VEHICLE ROUTE
 
 router.delete("/:id", middleware.checkVehicleOwnership, function(req,res){
     Vehicle.findByIdAndRemove(req.params.id, function(err){
