71088ef35641c7a03426678bdebc3da2fb583400
Andrew Tinsley <atinsley@astech-it.co.uk>
2020-01-14 11:31:06 +0000

    Updated Mongoose Version and connection  details

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+// Updated connection details
 mongoose.connect(process.env.AUTOREVIEWDATABASEURL,{ useNewUrlParser: true, useUnifiedTopology: true });
 //SeedDB();
 // PASSPORT CONFIG
